f66b6ea97defa8950e05cc4106ed74bf0d4a6c20
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2023-10-05 17:41:40 +0200

    next commit
    well-page not over

---
+++ heal-gamer/src/app/pages/anti-inflammatory-page/anti-inflammatory-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anti-inflammatory-page',
+  templateUrl: './anti-inflammatory-page.component.html',
+  styleUrls: ['./anti-inflammatory-page.component.css']
+})
+export class AntiInflammatoryPageComponent {
+
+}
--- heal-gamer/src/app/pages/pages.module.ts
+++ heal-gamer/src/app/pages/pages.module.ts
@@ -5,12 +5,16 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { LogInPageComponent } from './log-in-page/log-in-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { Routes, RouterModule } from '@angular/router';
+import { WellPageComponent } from './well-page/well-page.component';
+import { AntiInflammatoryPageComponent } from './anti-inflammatory-page/anti-inflammatory-page.component';
 
 const routesPages : Routes = [
   {path: '', redirectTo: '/main-page', pathMatch: 'full'},
   {path: 'main-page', component: MainPageComponent},
   {path: 'log-in-page', component: LogInPageComponent},
   {path: 'sign-up-page', component: SignUpPageComponent},
+  {path: 'well-page', component: WellPageComponent},
+  {path: 'anti-inflammatory-page', component: AntiInflammatoryPageComponent}
 ]
 
 @NgModule({
@@ -18,6 +22,8 @@ const routesPages : Routes = [
     MainPageComponent,
     LogInPageComponent,
     SignUpPageComponent,
+    WellPageComponent,
+    AntiInflammatoryPageComponent,
   ],
   imports: [
     FormsModule,
